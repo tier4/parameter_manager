e88c0cd11abdfd726ea21df4090d2181416d8c05
Yuma <yuma.nihei@tier4.jp>
2019-12-17 15:06:02 +0900

    first commit
    Signed-off-by: Yuma <yuma.nihei@tier4.jp>

---
+++ README.md
@@ -0,0 +1,1 @@
+## prameter_manager
---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# please set environmental variable "AW_PARAM_PATH" in ~/.bashrc
+source ~/.bashrc
+
+if [ -z $AW_PARAM_PATH ];then
+  echo "please set environment variable AW_PARAM_PATH"
+elif [ ! -d $AW_PARAM_PATH ];then
+  echo "cannot find $AW_PARAM_PATH"
+else
+  roscd parameter_manager || {
+    echo "failed to change directory"
+    return
+  }
+  if [ -e link ];then
+    rm -rf link
+  fi
+  ln -s $AW_PARAM_PATH link
+  if [ -e launch ];then
+    rm -rf launch
+  fi
+  mkdir -p launch
+  output_file=./launch/param.launch
+  target_files=`find ./link/* -name *.yaml`
+
+  echo "<!-- this file is automatically installed. -->" >> $output_file
+  echo "<launch>" >> $output_file
+  for file in $target_files
+  do
+    yaml_path=`echo $file|sed "s/\.\/link/\$\(find parameter_manager\)\/link/g"`
+    echo "  <rosparam"              >> $output_file
+    echo "    command=\"load\""     >> $output_file
+    echo "    file=\"$yaml_path\""  >> $output_file
+    echo "    subst_value=\"True\"" >> $output_file
+    echo "  />"                     >> $output_file
+  done
+  echo "</launch>" >> $output_file
+
+  echo "$AW_PARAM_PATH"
+  echo "success to install params!!!"
+fi
