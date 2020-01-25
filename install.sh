1951e4eb1931ba588a65a1a0e1b243249e9f26fe
Yuma Nihei <yuma.nihei@tier4.jp>
2020-01-25 09:16:15 +0900

    remove roscd & apply rosrun
    Signed-off-by: Yuma Nihei <yuma.nihei@tier4.jp>

--- install.sh
+++ install.sh
@@ -1,36 +1,53 @@
 #!/bin/bash
+#
+# input env vars:
+# - AW_INSTALL_PATH
+# - AW_PARAM_PATH
+# - AW_OPERATIONAL_TYPE
+#
+# please export the above vars in .profile and .bashrc
+#
 
-# please set environmental variable "AW_PARAM_PATH" in ~/.bashrc
-source ~/.bashrc
+source ${AW_INSTALL_PATH}/setup.sh
+current_dir=`pwd`
 
-if [ -z $AW_PARAM_PATH ];then
-  echo "please set environment variable AW_PARAM_PATH"
-elif [ ! -d $AW_PARAM_PATH ];then
-  echo "cannot find $AW_PARAM_PATH"
+autoware_parameter_dir=${AW_PARAM_PATH}/params.${AW_OPERATIONAL_TYPE}
+if [ -z "${autoware_parameter_dir}" ];then
+  echo "please set environment variable AW_PARAM_PATH & AW_OPERATIONAL_TYPE"
+elif [ ! -d ${autoware_parameter_dir} ];then
+  echo "cannot find ${autoware_parameter_dir}"
 else
-  roscd parameter_manager || {
-    echo "failed to change directory"
-    return
-  }
-  rm -rf link launch
-  ln -s $AW_PARAM_PATH link
-  mkdir -p launch
-  output_file=./launch/param.launch
-  target_files=`find ./link/* -name *.yaml`
+  install_dir=`rospack find parameter_manager`
+  if [ -e ${install_dir} ]; then
+    cd ${install_dir}
+    rm -rf link launch
+    ln -s ${autoware_parameter_dir} link
+    mkdir -p launch
+    output_file=./launch/param.launch
+    target_files=`find ./link/* -name *.yaml`
 
-  echo "<!-- this file is automatically installed. -->" >> $output_file
-  echo "<launch>" >> $output_file
-  for file in $target_files
-  do
-    yaml_path=`echo $file|sed "s/\.\/link/\$\(find parameter_manager\)\/link/g"`
-    echo "  <rosparam"              >> $output_file
-    echo "    command=\"load\""     >> $output_file
-    echo "    file=\"$yaml_path\""  >> $output_file
-    echo "    subst_value=\"True\"" >> $output_file
-    echo "  />"                     >> $output_file
-  done
-  echo "</launch>" >> $output_file
+    echo "<!-- this file is automatically installed. -->" >> ${output_file}
+    echo "<launch>" >> ${output_file}
+    for file in ${target_files}
+    do
+      yaml_path=`echo ${file}|sed "s/\.\/link/\$\(find parameter_manager\)\/link/g"`
+      echo "  <rosparam"                >> ${output_file}
+      echo "    command=\"load\""       >> ${output_file}
+      echo "    file=\"${yaml_path}\""  >> ${output_file}
+      echo "    subst_value=\"True\""   >> ${output_file}
+      echo "  />"                       >> ${output_file}
+      unset yaml_path
+    done
+    echo "</launch>" >> ${output_file}
 
-  echo "$AW_PARAM_PATH"
-  echo "success to install params!!!"
+    echo "${autoware_parameter_dir}"
+    echo "success to install params!!!"
+    unset output_file
+    unset target_files
+  fi
+  unset install_dir
 fi
+cd ${current_dir}
+unset current_dir
+unset autoware_parameter_dir
+
